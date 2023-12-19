f5074fce47bf6389c94022d30033746bcb382e69
Francisco Ortiz <jose.ortiz.ortiz@est.una.ac.cr>
2023-12-18 20:09:56 -0600

    Actualización del diseño del sideBar

---
+++ src/components/sidebar/sideBarOptions.tsx
@@ -0,0 +1,53 @@
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleIcon from '@mui/icons-material/People';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import FolderCopyIcon from '@mui/icons-material/FolderCopy';
+
+export interface Sublink {
+  href: string;
+  text: string;
+  icon: JSX.Element;
+  isActive: boolean;
+}
+
+export interface sideBarOption {
+  href?: string;
+  icon: JSX.Element;
+  text: string;
+  isActive?: boolean;
+  sublinks?: Sublink[];
+}
+
+export const sideBarOptions: sideBarOption[] = [
+  {
+    href: '/',
+    icon: <HomeIcon />,
+    text: 'Dashboard',
+    isActive: true,
+  },
+  {
+    icon: <PeopleIcon />,
+    text: 'Colaboradores',
+    sublinks: [
+      {
+        href: '/solicitudes',
+        text: 'Solicitudes',
+        icon: <TextSnippetIcon />,
+        isActive: false,
+      },
+      {
+        href: '/ausencias',
+        text: 'Ausencias',
+        icon: <AccessTimeIcon />,
+        isActive: false,
+      },
+      {
+        href: '/expediente',
+        text: 'Expedientes',
+        icon: <FolderCopyIcon />,
+        isActive: false,
+      },
+    ],
+  },
+];
--- src/components/tools/menu.tsx
+++ src/components/tools/menu.tsx
@@ -1,64 +1,28 @@
 import * as React from 'react';
-import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 
 import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import Button from '@mui/material/Button';
-import { useNavigate } from 'react-router-dom';
-
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import MenuIcon from '@mui/icons-material/Menu';
 
-import { useAuth } from '../../authProvider';
+
 import Rutas from '../../routes';
-import SidebarList from './sideBarOptions';
-import UsuarioService from '../../services/usuario.service';
-import { showConfirmation } from '../solicitudes/utils';
-
+import SideBar from '../sidebar/sideBar';
+import AccountPopover from './account';
 
 const drawerWidth = 240;
 
-const usuarioService = new UsuarioService();
 
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'flex-end',
   padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
@@ -84,108 +48,36 @@ const AppBar = styled(MuiAppBar, {
   }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-  }),
-);
 
 export default function MenuPanel() {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const { userRole, setLoggedIn } = useAuth();
-  const navigate = useNavigate();
+  
+  const [open] = React.useState(false);
+  const [ isOpen, setIsOpen ] = React.useState(false);
+  const [isOpenState, setIsOpenState] = React.useState<Record<string, boolean>>(() => {
+    const storedOpenState = localStorage.getItem('isOpenState');
+    return storedOpenState ? JSON.parse(storedOpenState) : {};
+  });
 
-  const settings = [
-    { icon: null, text: 'Administración', onClick: () => navigate('/administrador'), role: 'admin'  },
-    { icon: null, text: 'Cuenta', onClick: () => navigate('/')  },
-    { icon: null, text: 'Dashboard', onClick: () => navigate('/')  },
-    { icon: null, text: 'Cerrar sesión', onClick: () => handleLogout() }
-  ];
-
-  const filteredSettings = settings.filter(setting => !setting.role || setting.role === userRole);
-
-
-  const opciones = [
-    { icon: null, text: '', link: '/' },
-    { icon: null, text: '', link: '/' },
-    { icon: null, text: '', link: '/' },
-  ];
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
+  const toggleSideBar = () => {
+    setIsOpen(!isOpen);
   };
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  const handleLogout = async () =>{
-    handleCloseUserMenu();
-    const confirmed = await showConfirmation();
-    if (confirmed){
-      const token = localStorage.getItem('refreshToken');
-
-      if(token){
-        const response = await usuarioService.logout(token);
-
-          if(response.status === 200){
-            setLoggedIn(false);
-            localStorage.removeItem('accessToken');
-            localStorage.removeItem('refreshToken');
-            sessionStorage.removeItem('Welcome');
-            navigate('');
-          }    
-      }
-    }
-
-  }
+  React.useEffect(() => {
+    localStorage.setItem('isOpenState', JSON.stringify(isOpenState));
+    
+    return () =>{
+      localStorage.removeItem('isOpenState');
+    };
+  }, [isOpenState]);
 
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
         <Toolbar sx={{backgroundColor: 'rgb(27,61,81)'}} >
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{
-              marginRight: 5,
-              ...(open && { display: 'none' }),
-            }}
-          >
-            <MenuOpenIcon />
-          </IconButton>
+          <div className='button'  onClick={toggleSideBar}>
+            {!isOpen ? <MenuOpenIcon /> : <MenuIcon />}
+          </div>
           <Typography
             variant="h6"
             noWrap
@@ -193,6 +85,7 @@ export default function MenuPanel() {
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
+              flexGrow: 1,
               fontFamily: 'monospace',
               fontWeight: 700,
               letterSpacing: '.3rem',
@@ -202,43 +95,6 @@ export default function MenuPanel() {
           >
             SAAG
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MoreVertIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {opciones.map((page, index) => (
-                <MenuItem key={index}  onClick={() => navigate(page.link) }>
-                    {page.icon}
-                  <Typography textAlign="center">{page.text}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
           <Typography
             variant="h5"
             noWrap
@@ -256,63 +112,15 @@ export default function MenuPanel() {
           >
             SAAG
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {opciones.map((page,index) => (
-              <Button
-                key={index}
-                onClick={() => navigate(page.link) }
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              > 
-                {page.icon}
-                {page.text}
-              </Button>
-            ))}
-          </Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Cuenta">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <AccountCircleIcon fontSize="large" style={{ color: 'white' }}/>
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {filteredSettings.map((setting, index) => (
-                <MenuItem key={index} onClick={() => setting.onClick()}>
-                  <Typography textAlign="center">{setting.text}</Typography>
-                  {setting.icon}
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+            <Box>
+              <AccountPopover/>
+            </Box>
         </Toolbar>
       </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <SidebarList open={open} />
-        <Divider/>
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
-        <Rutas/>
+      <SideBar  isOpen={isOpen} isOpenState={isOpenState} setIsOpenState={setIsOpenState}/>
+      <Box component="main" sx={{ flexGrow: 1, p: 3, marginLeft: !isOpen ? '260px' : '95px' }} className="main-content">
+          <DrawerHeader />
+          <Rutas />
       </Box>
     </Box>
   );
